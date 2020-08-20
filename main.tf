4b54784a4ba305a106ed00856875acd83f25ef3e
Andrew Oteiza <oteizaa@gmail.com>
2020-08-19 23:32:41 -0300

    third commit

---
+++ main.tf
@@ -0,0 +1,50 @@
+# Main
+
+# Terraform Version Pinning
+terraform {
+    required_version = "~> 0.13"
+    required_providers {
+        azurerm = "~> 2.1.0"
+    }
+}
+
+# Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = var.sp_subscription_id
+  client_id       = var.sp_client_id
+  client_secret   = var.sp_client_secret
+  tenant_id       = var.sp_tenant_id
+}
+
+# Create a Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}_rg"
+  location = var.location
+}
+
+# Create Log Analytic Workspace
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "${var.prefix}-law"
+  sku                 = "PerNode"
+  retention_in_days   = 300
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+}
+
+# Create the Storage Account
+resource "azurerm_storage_account" "mystorage" {
+  name                     = "${var.prefix}mystorage"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment             = var.environment
+    owner                   = var.owner
+    group                   = var.group
+    costcenter              = var.costcenter
+    application             = var.application
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,13 @@
+# BIG-IP Environment
+uname     = "azureuser"
+upassword = "f5DEMOs4u#"
+
+# Azure Environment
+sp_subscription_id = ""
+sp_client_id       = ""
+sp_client_secret   = ""
+sp_tenant_id       = ""
+location           = "westus2"
+
+# Prefix for objects being created
+prefix = "apiprotection123"
